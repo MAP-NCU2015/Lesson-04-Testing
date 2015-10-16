236c051d0997521e62e8be7fd24e46d5f573d3e9
askeing <askeing@gmail.com>
2015-10-16 16:57:08 +0800

    broke the test

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -1,10 +1,11 @@
 var assert = require('assert');
 var cal = require('../calculator.js')
 
+assert.equal('1','2')
 describe('Calculator', function(){
   describe('add', function(){
     it('add', function(){
-      assert.equal('2', cal.add(1, 1));
+      assert.equal('3', cal.add(1, 1));
     })
   })
 })
