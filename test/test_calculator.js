ffbabc8a9786654fa23b713bbddf8fb355b5cbb1
Shark <sharknevercries@gmail.com>
2015-10-19 19:55:10 +0800

    Add test of addition and substraction

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -2,9 +2,30 @@ var assert = require('assert');
 var cal = require('../calculator.js')
 
 describe('Calculator', function(){
+
   describe('add', function(){
-    it('add', function(){
+    
+    it('add1', function(){
       assert.equal('2', cal.add(1, 1));
     })
+
+    it('add2', function(){
+    	assert.equal('2.596', cal.add(1.236, 1.360));
+    })
+
   })
+
+  describe('substract', function(){
+
+  	it('substract1', function(){
+  		assert.equal('-5', cal.substract(0, 5));
+  	})
+
+  	it('substract2', function(){
+  		assert.equal('-2.354', cal.substract(1.352, 3.706));
+  	})
+
+  })
+
+  
 })
