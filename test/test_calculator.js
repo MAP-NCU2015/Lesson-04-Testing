8072a5084054a263521132bee7d497416ff51cd0
caesernieh <githubtry@gmail.com>
2015-10-22 23:26:03 +0800

    modify test case

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -3,8 +3,30 @@ var cal = require('../calculator.js')
 
 describe('Calculator', function(){
   describe('add', function(){
-    it('add', function(){
-      assert.equal('2', cal.add(1, 1));
+    it('add1', function(){
+      assert.equal('1.791', cal.add(1.555, 0.236));
+    })
+    it('add2',function(){
+      assert.equal('5',cal.add(4 , 1));
+    })
+    it('substract1',function(){
+      assert.equal('4', cal.substract(6,2));
+    })
+    it('substract2',function(){
+      assert.equal('18',cal.substract(20 , 2));
+    })
+    it('multilpy1',function(){
+      assert.equal('12', cal.multilply(6,2));
+    })
+    it('multilpy2',function(){
+      assert.equal('18.492',cal.multilply(5.36,3.45));	
+    })
+    it('divide1',function(){
+      assert.equal('2',cal.divide(6 , 3));
+    })
+    it('Input',function(){
+      assert.equal(true,cal.isNumber(1.2345));
     })
   })
 })
+
