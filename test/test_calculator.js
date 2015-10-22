2d685ceaad49e606726488a231209161cc49bf52
eric <eric@127.0.0.1 localhost.localdomain>
2015-10-22 09:17:57 -0400

    multiplication and division added

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -2,9 +2,18 @@ var assert = require('assert');
 var cal = require('../calculator.js')
 
 describe('Calculator', function(){
-  describe('add', function(){
+  describe('operater test', function(){
     it('add', function(){
-      assert.equal('2', cal.add(1, 1));
+      assert.equal('7', cal.add(3, 4));
     })
+    it('substract', function(){
+      assert.equal('-1', cal.substract(3, 4));
+    })
+    it('multiply', function(){
+      assert.equal('12', cal.multiply(3, 4));
+    })
+    it('divide', function(){
+      assert.equal('2', cal.divide(4, 2));
+    })  
   })
 })
