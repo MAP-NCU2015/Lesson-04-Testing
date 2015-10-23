342292b1e6ce50e5be79f4094fc559414a3e0470
esxleader <esxleader@gmail.com>
2015-10-23 14:41:03 +0800

    HW4-V2

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -7,7 +7,7 @@ describe('Calculator', function(){
     it('add', function(){
       assert.equal('2', cal.add(1, 1));
     })
-   
+    
     it('substract', function(){
       assert.equal('10', cal.substract(20, 10));
     })
@@ -16,13 +16,13 @@ describe('Calculator', function(){
       assert.equal('12', cal.multiply(3, 4));
     })
   
-  it('devide', function(){
+    it('devide', function(){
       assert.equal('2', cal.devide(20, 10));
     })
  })
   describe('Unvalid Input', function(){
         it('not number', function(){
             assert.equal(false, cal.isNumber('ABC'))
-       })
+        })
     })
- })
+})
