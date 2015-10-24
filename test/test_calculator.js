e8a8efa285339004cf026d46a610cf3eabc530e4
張勝韋 <g84569@yahoo.com.tw>
2015-10-25 01:20:52 +0800

    add mul/dev; fix reg; modify test;

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -2,9 +2,26 @@ var assert = require('assert');
 var cal = require('../calculator.js')
 
 describe('Calculator', function(){
-  describe('add', function(){
+  describe('ComputeTest', function(){
     it('add', function(){
       assert.equal('2', cal.add(1, 1));
     })
+    it('subtract', function () {
+      assert('2',cal.sub(4,2))
+    })
+    it('multiply',function()
+    {
+      assert('4',cal.mul(2,2))
+    })
+    it('devide',function()
+    {
+      assert('1',cal.dev(2,2))
+    })
+  })
+  describe('NotNumberTest', function(){
+    it('notNumber', function(){
+      assert.equal(false, cal.isNumber('a'));
+    })
+
   })
 })
