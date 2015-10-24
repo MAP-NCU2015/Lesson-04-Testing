6e534cf8e626b696f99e37393359634ef4089964
Peter <peter654qq@gmail.com>
2015-10-24 18:28:26 +0800

    add new tests (positive and negative path test)

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -6,5 +6,20 @@ describe('Calculator', function(){
     it('add', function(){
       assert.equal('2', cal.add(1, 1));
     })
+	it('add', function(){
+      assert.equal('ERROR', cal.add(a, b));
+    })
+	it('substract', function(){
+      assert.equal('4', cal.substract(7, 3));
+    })
+	it('multiply', function(){
+      assert.equal('21', cal.multiply(7, 3));
+    })
+	it('divide', function(){
+      assert.equal('3', cal.divide(9, 3));
+    })
+	it('module', function(){
+      assert.equal('0', cal.module(9, 3));
+    })
   })
 })
