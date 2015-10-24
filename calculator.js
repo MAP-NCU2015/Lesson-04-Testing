52dd460a1fc8336b7a889f03d863379bee784e24
JACK <jack99541008@gmail.com>
2015-10-24 21:10:18 +0800

    hw3

--- calculator.js
+++ calculator.js
@@ -11,5 +11,12 @@ function substract(num1, num2){
   return num1 - num2;
 }
 
+function mul(num1, num2){
+  return num1 * num2;
+}
+
+function div(num1, num2){
+  return num1 / num2;
+}
 module.exports.add = add;
 module.exports.isNumber = isNumber;
--- test/test_calculator.js
+++ test/test_calculator.js
@@ -7,4 +7,19 @@ describe('Calculator', function(){
       assert.equal('2', cal.add(1, 1));
     })
   })
+  describe('substract', function(){
+    it('substract', function(){
+      assert.equal('0', cal.substract(1, 1));
+    })
+  })
+  describe('mul', function(){
+    it('mul', function(){
+      assert.equal('1', cal.mul(1, 1));
+    })
+  })
+  describe('div', function(){
+    it('div', function(){
+      assert.equal('1', cal.div(1, 1));
+    })
+  })
 })
