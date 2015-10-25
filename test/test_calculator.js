1ac56943dd6fd04ceb578cc021d68881ff90a0e4
RajonRubio <xab39654321@gmail.com>
2015-10-25 17:42:06 +0800

    home work

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -2,9 +2,26 @@ var assert = require('assert');
 var cal = require('../calculator.js')
 
 describe('Calculator', function(){
-  describe('add', function(){
+  describe('Basic operation', function(){
     it('add', function(){
-      assert.equal('2', cal.add(1, 1));
+      assert.equal('600', cal.add(100, 500));
     })
+    it('substract', function(){
+      assert.equal('200', cal.substract(500, 300));
+    })
+    it('multiply', function(){
+      assert.equal('50000', cal.multiply(100, 500));
+    })
+    it('divide', function(){
+      assert.equal('70', cal.divide(700, 10));
+    })
+    it('remainder', function(){
+      assert.equal('299', cal.remainder(999, 700));
+    })
+  })
+  describe('Unvalid Input', function(){
+        it('not number', function(){
+            assert.equal(false, cal.isNumber('HEFYUHUEFH'))
+        })
   })
 })
