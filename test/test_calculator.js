46902f7d51b934a48b3ac3c10b4a17cc233aca72
JasonChiu <jason1122g@gmail.com>
2015-10-25 16:15:58 +0800

    add new tests then add new feature

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -3,8 +3,18 @@ var cal = require('../calculator.js')
 
 describe('Calculator', function(){
   describe('add', function(){
-    it('add', function(){
+    it('add correct value', function(){
       assert.equal('2', cal.add(1, 1));
+    })
+  })
+  describe('substract', function(){
+    it('substract correct value', function(){
+      assert.equal('0', cal.substract(1, 1));
+    })
+  })
+  describe('multiply', function(){
+    it('multiply correct value', function(){
+      assert.equal('1', cal.multiply(1, 1));
     })
   })
 })
