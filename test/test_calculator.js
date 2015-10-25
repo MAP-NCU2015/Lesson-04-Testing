905f4d21386778e732f143054dfa48f546209616
mike19941215 <tachibana3151@gmail.com>
2015-10-25 15:03:21 +0800

    Negative path

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -2,27 +2,25 @@ var assert = require('assert');
 var cal = require('../calculator.js')
 
 describe('Calculator', function(){
-  describe('add', function(){
     it('add', function(){
       assert.equal('2', cal.add(1, 1));
     })
-  })
   
-  describe('substract', function(){
 	it('substract',function(){
 	   assert.equal('1' , cal.substract(2,1));
 	})
-  })
   
-  describe('mul', function(){
 	it('mul',function(){
 	   assert.equal('6' , cal.mul(2,3));
 	})
-  })
   
-  describe('div', function(){
 	it('div',function(){
 	   assert.equal('0.5' , cal.div(1,2));
 	})
-  })
+})
+
+describe('Negative path', function(){
+	it('isNumber',function(){
+		assert.equal(false,cal.isNumber('13A'));
+	})
 })
