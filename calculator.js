5ccb19d45b1d2d7ab0f0a6fc630f5f246ece3c79
DonQuijoTeDe <q910553@gmail.com>
2015-10-27 13:02:48 +0800

    except bonus

--- calculator.js
+++ calculator.js
@@ -1,15 +1,31 @@
-function isNumber(numStr){
-  var re = /^[0-9\.]+$/; 
-  return re.test(numStr);
+function isNumber(numStr) {
+    var re = /^[0-9\.]+$/;
+    return re.test(numStr);
 }
 
-function add(num1, num2){
-  return num1 + num2;
+function add(num1, num2) {
+    return num1 + num2;
 }
 
-function substract(num1, num2){
-  return num1 - num2;
+function substract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function random(num1, num2) {
+    return Math.floor(Math.random() * (num2 - num1)) + num1;
 }
 
 module.exports.add = add;
+module.exports.substract = substract;
+module.exports.multiply = multiply;
+module.exports.divide = divide;
 module.exports.isNumber = isNumber;
+module.exports.random = random;
--- test/test_calculator.js
+++ test/test_calculator.js
@@ -1,10 +1,32 @@
 var assert = require('assert');
 var cal = require('../calculator.js')
+var sinon = require('../node_modules/sinon/lib/sinon.js')
 
-describe('Calculator', function(){
-  describe('add', function(){
-    it('add', function(){
-      assert.equal('2', cal.add(1, 1));
+describe('Calculator', function () {
+    describe('positive test', function () {
+        it('add', function () {
+            assert.equal('12', cal.add(10, 2));
+        })
+        it('substract', function () {
+            assert.equal('8', cal.substract(10, 2));
+        })
+        it('multiply', function () {
+            assert.equal('20', cal.multiply(10, 2));
+        })
+        it('divide', function () {
+            assert.equal('5', cal.divide(10, 2));
+        })
     })
-  })
+    describe('negative test', function () {
+        it('isnumber', function () {
+            assert.equal(false, cal.isNumber("abc"));
+        })
+    })
+    describe('work with sinon.js', function () {
+        it('stub with the random number', function () {
+            var stub = sinon.stub(cal, "random").returns(50);
+            var callback = stub();
+            assert.equal('100', cal.add(50, callback));
+        })
+    })
 })
