ef0c167e38424bb8bf34625e15e288c09a712c72
Rueian Oneecom <rueiancsie@gmail.com>
2015-10-27 23:02:32 +0800

    Complete positive tests

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -7,4 +7,16 @@ describe('Calculator', function(){
       assert.equal('2', cal.add(1, 1));
     })
   })
+
+  describe('substract', function(){
+    it('substract', function(){
+      assert.equal('2', cal.substract(3, 1));
+    })
+  })
+
+  describe('isNumber', function(){
+    it('isNumber', function(){
+      assert.equal(true, cal.isNumber('3'));
+    })
+  })
 })
