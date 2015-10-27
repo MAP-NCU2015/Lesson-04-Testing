d7234842826149252ce73e638dc13a62fa68ceea
Rueian Oneecom <rueiancsie@gmail.com>
2015-10-27 23:31:12 +0800

    Add random method and test

--- calculator.js
+++ calculator.js
@@ -15,7 +15,12 @@ function multiply(num1, num2){
   return num1 * num2;
 }
 
+function randomAdd(num) {
+  return Math.random() + num;
+}
+
 module.exports.add = add;
 module.exports.substract = substract;
 module.exports.multiply = multiply;
+module.exports.randomAdd = randomAdd;
 module.exports.isNumber = isNumber;
--- test/test_calculator.js
+++ test/test_calculator.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var sinon = require('sinon');
 var cal = require('../calculator.js')
 
 describe('Calculator', function(){
@@ -27,6 +28,13 @@ describe('Calculator', function(){
   describe('multiply', function(){
     it('multiply', function(){
       assert.equal(6, cal.multiply(3, 2));
+    })
+  })
+
+  describe('randomAdd', function(){
+    it('randomAdd', function(){
+      var cb = sinon.stub(Math, 'random').returns(0.1);
+      assert.equal(1.1, cal.randomAdd(1));
     })
   })
 })
