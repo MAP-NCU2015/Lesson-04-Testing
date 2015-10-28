ebc3a1713022705c7b41b13fb2ee0def9c7c1b84
vickychen95 <vickychen95@gmail.com>
2015-10-29 01:47:13 +0800

    added new features

--- calculator.js
+++ calculator.js
@@ -7,9 +7,20 @@ function add(num1, num2){
   return num1 + num2;
 }
 
-function substract(num1, num2){
+function subtract(num1, num2){
   return num1 - num2;
 }
 
+function multiply(num1, num2){
+	return num1*num2;
+}
+
+function divide(num1, num2){
+	return num1/num2;
+}
+
 module.exports.add = add;
+module.exports.subtract = subtract;
+module.exports.multiply = multiply;
+module.exports.divide = divide;
 module.exports.isNumber = isNumber;
--- test/test_calculator.js
+++ test/test_calculator.js
@@ -4,7 +4,23 @@ var cal = require('../calculator.js')
 describe('Calculator', function(){
   describe('add', function(){
     it('add', function(){
-      assert.equal('2', cal.add(1, 1));
+      assert.equal('5.73', cal.add(4.5, 1.23));
+    })
+	describe('subtract', function(){
+    it('subtract', function(){
+      assert.equal('0', cal.subtract(1, 1));
+    })
+	describe('multiply', function(){
+    it('multiply', function(){
+      assert.equal('3.6', cal.multiply(3, 1.2));
+    })
+	describe('divide', function(){
+    it('divide', function(){
+      assert.equal('3', cal.divide(24, 8));
+    })
+	describe('divide', function(){
+    it('divide', function(){
+      assert.equal('ERROR: divisor can't be zero.', cal.divide(24, 0));
     })
   })
 })
