92d0969698266b72b31304a06b19eab30ae5b35f
jim83531 <jim83531@gmail.com>
2015-10-29 23:20:30 +0800

    HW4

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -6,5 +6,38 @@ describe('Calculator', function(){
     it('add', function(){
       assert.equal('2', cal.add(1, 1));
     })
+	it('add-negative', function(){
+	  assert.equal('-3', cal.mul(-1, -2));
+	})
+  })
+})
+describe('Calculator', function(){
+  describe('sub', function(){
+    it('sub', function(){
+      assert.equal('3', cal.sub(4, 1));
+    })
+	it('sub-negative', function(){
+	  assert.equal('-5', cal.mul(-1, 4));
+	})
+  })
+})
+describe('Calculator', function(){
+  describe('mul', function(){
+    it('mul', function(){
+      assert.equal('2', cal.mul(1, 2));
+    })
+	it('mul-negative', function(){
+	  assert.equal('-1', cal.mul(-1,1));
+	})
+  })
+})
+describe('Calculator', function(){
+  describe('div', function(){
+    it('div', function(){
+      assert.equal('3', cal.div(3, 1));
+    })
+	it('div-negative', function(){
+	  assert.equal('-3', cal.mul(-1,1));
+	})
   })
 })
