8bcc7582018d308e0af4aa08ac606a68817bd6ff
kaofushwai <panhsuanjen@gmail.com>
2015-10-30 00:41:58 +0800

    HW4

--- calculator.js
+++ calculator.js
@@ -11,5 +11,15 @@ function substract(num1, num2){
   return num1 - num2;
 }
 
+function multiply(num1 ,num2){
+return num1*num2;
+}
+
+function divide(num1 , num2){
+retuen num1/num2;
+}
 module.exports.add = add;
+module.exports.substract = substract;
+module.exports.multiply = multiply;
+module.exports.divide = divide;
 module.exports.isNumber = isNumber;
--- test/test_calculator.js
+++ test/test_calculator.js
@@ -4,7 +4,29 @@ var cal = require('../calculator.js')
 describe('Calculator', function(){
   describe('add', function(){
     it('add', function(){
-      assert.equal('2', cal.add(1, 1));
+      assert.equal('8', cal.add(3, 5));
     })
   })
+  describe('substract',function(){
+	it('substract',function(){
+		assert.equal('-2',cal.substract(3,5));
+	})
+  })
+  describe('multiply',function(){
+	it('multiply',function(){
+		assert.equal('15',cal.multiply(3,5));
+	})
+  })
+  describe('divide', function(){
+    it('divide', function(){
+      assert.equal('5', cal.divide(15, 3));
+    })
+  })
+  describe('isNumber', function(){
+    it('isNumber', function(){
+      assert.equal('1', cal.isNumber(-1));
+      assert.equal('0', cal.isNumber('ABC'));
+    })
+  })
+  s
 })
