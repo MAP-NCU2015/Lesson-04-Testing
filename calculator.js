ea5c98b69a3384bd8001d00246fe33c8639b7000
RichardYang <ylin0603@gmail.com>
2015-10-29 21:18:22 +0800

    Finish sinon

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 function isNumber(numStr){
-  var re = /^[0-9\.]+$/; 
+  var re=/^[+-]?[0-9]+\.{0,1}[0-9]*$/
   return re.test(numStr);
 }
 
@@ -10,6 +10,14 @@ function add(num1, num2){
 function substract(num1, num2){
   return num1 - num2;
 }
-
+function mul(num1,num2){
+    return num1*num2;
+}
+function div(num1,num2){
+    return num1/num2;
+}
+module.exports.substract=substract;
+module.exports.div=div;
+module.exports.mul=mul;
 module.exports.add = add;
 module.exports.isNumber = isNumber;
