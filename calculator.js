07643635bd91af5923c1b7f49251fd8fe8f195cc
jnshao <kick2007sky@gmail.com>
2015-10-29 23:02:41 +0800

    Add mult, divide functions. Can calculate negative number. Test case: has positive path and negative path

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 function isNumber(numStr){
-  var re = /^[0-9\.]+$/; 
+  var re = /^[\-]?[0-9]+[\.]?[0-9]*$/; //can cal for negative number
   return re.test(numStr);
 }
 
@@ -9,6 +9,14 @@ function add(num1, num2){
 
 function substract(num1, num2){
   return num1 - num2;
+}
+
+function mult(num1, num2){
+	return num1 * num2;
+}
+
+function divide(num1, num2){
+	return num1 / num2;
 }
 
 module.exports.add = add;
--- test/test_calculator.js
+++ test/test_calculator.js
@@ -5,6 +5,20 @@ describe('Calculator', function(){
   describe('add', function(){
     it('add', function(){
       assert.equal('2', cal.add(1, 1));
+      assert.equal('6', cal.add(3, 3));
+      assert.equal('ERROR', cal.add(1.5.5, 0)); //negative path test
+    })
+  })
+  describe('mult', function(){
+    it('mult', function(){
+      assert.equal('0', cal.mult(0, 1));
+      Infinity
+    })
+  })
+  describe('divide', function(){
+    it('divide', function(){
+      assert.equal('0', cal.mult(0, 1));
+      assert.equal('Infinity', cal.mult(5, 0));
     })
   })
 })
