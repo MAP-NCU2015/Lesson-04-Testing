13a9d9a4537973bede0f004efd8d2fbfa1717c15
kikiyin <yinchung83@gmail.com>
2015-10-29 16:00:05 +0800

    WebApp_HW4

--- calculator.js
+++ calculator.js
@@ -11,5 +11,22 @@ function substract(num1, num2){
   return num1 - num2;
 }
 
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function devide(num1, num2){
+	return num1 / num2;
+}
+
+function random(){
+	return Math.floor((Math.random() * 100) + 1);
+}
+
+// exports method for other modules or files to use
 module.exports.add = add;
 module.exports.isNumber = isNumber;
+module.exports.substract = substract;
+module.exports.multiply = multiply;
+module.exports.devide = devide;
+module.exports.random = random;
