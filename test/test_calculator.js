618cf30412db3f3dfc630485edb3981ffe4216c4
RichardYang <ylin0603@gmail.com>
2015-10-30 14:47:06 +0800

    try selenium

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var cal = require('../calculator.js')
 var sinon=require('sinon')
+var webdriver = require('selenium-webdriver');
 describe('Calculator', function(){
     describe('add', function(){
         it('add', function(){
@@ -55,13 +56,34 @@ describe('Calculator', function(){
     describe('negative_test', function(){
         it('shold be false', function(){
             assert.equal(false, cal.isNumber('a'));
+            assert.equal(true, cal.isNumber(1));
         })
     })
     describe('sinon_test',function(){
         it('Sinon test',function(){
-        var stub = sinon.stub(Math,"random").returns(39);
+            var stub = sinon.stub(Math,"random").returns(39);
 
-        assert.equal(39,cal.mul(Math.random(),1));
+            assert.equal(39,cal.mul(Math.random(),1));
         })
     })
+    describe('selenium-webdriver',function(){
+        it('selenium-test',function(done){
+            var driver = new webdriver.Builder().
+                forBrowser('chrome').build();
+                driver.get("http://127.0.0.1:8000/")
+                var searchBox=driver.findElement(webdriver.By.id('num1'));
+                searchBox.sendKeys('1');
+                var searchBox2=driver.findElement(webdriver.By.id('num2'));
+                searchBox2.sendKeys('1');
+                driver.findElement(webdriver.By.id('go_btn')).click();
+                var searchBoxResult = driver.findElement(webdriver.By.id('ans'));
+                searchBoxResult.getAttribute('value').then(function(value){
+                    console.log(value);
+                    assert.equal(value,'3');
+                }).then(done());
+                //driver.quit();    
+        })
+    })
+
+
 })
