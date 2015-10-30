da8da0e6d414887780d869aa4fa56e524711a124
Anchorboy <s810142000@gmail.com>
2015-10-30 15:18:54 +0800

    first test done

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function isNumber(numStr){
+  var re = /^[0-9\.]+$/; 
+  return re.test(numStr);
+}
+
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function substract(num1, num2){
+  return num1 - num2;
+}
+
+module.exports.add = add;
+module.exports.isNumber = isNumber;
