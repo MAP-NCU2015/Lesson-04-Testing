760b6a5fe91d1386385bb944839474b8c49984d0
henryma68 <a0936127996@gmail.com>
2015-10-30 23:53:52 +0800

    ver1

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -1,10 +1,17 @@
 var assert = require('assert');
 var cal = require('../calculator.js')
 
-describe('Calculator', function(){
-  describe('add', function(){
-    it('add', function(){
-      assert.equal('2', cal.add(1, 1));
+describe('Calculator', function () {
+    describe('add_positive', function () {
+        it('add', function () {
+            assert.equal('2', cal.add(1, 1));
+
+        })
     })
-  })
+    describe('add_nagative', function () {
+        it('add', function () {
+            assert.equal('-2', cal.add(-1, -1));
+
+        })
+    })
 })
