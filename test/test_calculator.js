24ed8e8348ac6fb944110754b03d08bd25010425
vickychen95 <vickychen95@gmail.com>
2015-10-30 14:27:50 +0800

    fix test

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -6,15 +6,18 @@ describe('Calculator', function(){
     it('add', function(){
       assert.equal('5.73', cal.add(4.5, 1.23));
     })
-	describe('subtract', function(){
+  })
+  describe('subtract', function(){
     it('subtract', function(){
       assert.equal('0', cal.subtract(1, 1));
     })
-	describe('multiply', function(){
+  })
+  describe('multiply', function(){
     it('multiply', function(){
       assert.equal('3.75', cal.multiply(3, 1.25));
     })
-	describe('divide', function(){
+  })
+  describe('divide', function(){
     it('divide', function(){
       assert.equal('3', cal.divide(24, 8));
     })
