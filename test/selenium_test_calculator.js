8a154834d10a974686b26cf1ad0821737d28d857
catLee <leemiyinghao@gmail.com>
2015-10-30 22:07:32 +0800

    Second try for Selenium testing local file on CI server.

--- test/selenium_test_calculator.js
+++ test/selenium_test_calculator.js
@@ -1,12 +1,11 @@
 var assert = require('assert');
 var webdriver = require('selenium-webdriver');
-
+var SeleniumServer = require('selenium-webdriver/remote').SeleniumServer;
 describe('Calculator-Selenium', function() {
     it('add', function(done){
 	this.timeout(60000);
 	driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
-	driver.get('http://127.0.0.1:8000');
-	//Todo: Automatic Launch Http server
+	driver.get("file:///" + __dirname + "/../index.html");
 	driver.wait(function(){
 	    driver.findElement(webdriver.By.id('num1')).sendKeys('23');
 	    driver.findElement(webdriver.By.id('num2')).sendKeys('00');
