f59a422c96afb686a50f0e538ecd1fb2f13c5a18
catLee <leemiyinghao@gmail.com>
2015-10-30 20:59:30 +0800

    finish Selenium test

---
+++ test/selenium_test_calculator.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var webdriver = require('selenium-webdriver');
+
+describe('Calculator-Selenium', function() {
+    it('add', function(done){
+	this.timeout(60000);
+	driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
+	driver.get('http://127.0.0.1:8000');
+	//Todo: Automatic Launch Http server
+	driver.wait(function(){
+	    driver.findElement(webdriver.By.id('num1')).sendKeys('23');
+	    driver.findElement(webdriver.By.id('num2')).sendKeys('00');
+	    driver.findElement(webdriver.By.id('go_btn')).click();
+	    var ans = driver.findElement(webdriver.By.id('ans')).getAttribute('value').then(function(value){
+		assert.equal(value, 223);
+		done();
+	    });
+	},3000).then(done);
+    });
+});
