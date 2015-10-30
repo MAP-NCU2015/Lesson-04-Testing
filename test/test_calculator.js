1b7afa0f6132e7aa9197ac9a11358bfeb2de5662
jim83531 <jim83531@gmail.com>
2015-10-30 09:27:11 +0800

    FIX MISTAKE #2

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -7,7 +7,10 @@ describe('Calculator', function(){
       assert.equal('2', cal.add(1, 1));
     })
 	it('add-negative', function(){
-	  assert.equal('-3', cal.mul(-1, -2));
+	  assert.equal('-3', cal.add(-1, -2));
+	})
+	it('add-decimal_point', function(){
+	  assert.equal('2.3', cal.add(1.1, 1.2));
 	})
   })
 })
@@ -17,7 +20,10 @@ describe('Calculator', function(){
       assert.equal('3', cal.sub(4, 1));
     })
 	it('sub-negative', function(){
-	  assert.equal('-5', cal.mul(-1, 4));
+	  assert.equal('-5', cal.sub(-1, 4));
+	})
+	it('sub-decimal_point', function(){
+	  assert.equal('3.4', cal.sub(4.0, 0.6));
 	})
   })
 })
@@ -29,6 +35,9 @@ describe('Calculator', function(){
 	it('mul-negative', function(){
 	  assert.equal('-1', cal.mul(-1, 1));
 	})
+	it('mul-decimal_point', function(){
+	  assert.equal('0.25', cal.mul(0.5, 0.5));
+	})
   })
 })
 describe('Calculator', function(){
@@ -37,7 +46,10 @@ describe('Calculator', function(){
       assert.equal('3', cal.div(3, 1));
     })
 	it('div-negative', function(){
-	  assert.equal('-3', cal.mul(3, -1));
+	  assert.equal('-3', cal.div(3, -1));
+	})
+	it('div-decimal_point', function(){
+	  assert.equal('0.3', cal.div(0.6, 2));
 	})
   })
 })
