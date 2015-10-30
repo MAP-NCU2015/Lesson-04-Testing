2b40b108a117ad0c8bf0992f9ce6d680cefd96f2
DannyJJJJ <danny89019@gmail.com>
2015-10-30 12:39:02 +0800

    Update test_calculator.js

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -2,9 +2,23 @@ var assert = require('assert');
 var cal = require('../calculator.js')
 
 describe('Calculator', function(){
-  describe('add', function(){
+  describe('All Operator', function(){
     it('add', function(){
       assert.equal('2', cal.add(1, 1));
     })
+    it('substract', function(){
+      assert.equal('0', cal.substract(1, 1));
+    })
+    it('mult', function(){
+      assert.equal('4', cal.mult(2, 2));
+    })
+    it('div', function(){
+      assert.equal('2', cal.div(2, 1));
+    })
   })
+  
+  describe('Not a number', function(){
+	  it('NotaNumber', function(){
+      assert.equal(false, cal.isNumber('a'));
+   })
 })
