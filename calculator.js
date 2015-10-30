7a4d1133b398da8c0e2388105cf537bb32998259
Anchorboy <s810142000@gmail.com>
2015-10-30 18:00:01 +0800

    finish basic requirements

--- calculator.js
+++ calculator.js
@@ -11,5 +11,21 @@ function substract(num1, num2){
   return num1 - num2;
 }
 
+function multiply(num1, num2){
+	return num1 * num2;
+}
+
+function divide(num1, num2){
+	return num1 / num2;
+}
+
+function mod(num1, num2){
+	return num1 % num2;
+}
+
 module.exports.add = add;
+module.exports.substract = substract;
+module.exports.multiply = multiply;
+module.exports.divide = divide;
+module.exports.mod = mod;
 module.exports.isNumber = isNumber;
--- test/test_calculator.js
+++ test/test_calculator.js
@@ -6,5 +6,29 @@ describe('Calculator', function(){
     it('add', function(){
       assert.equal('2', cal.add(1, 1));
     })
+  }),
+
+  describe('substract', function(){
+    it('substract', function(){
+      assert.equal('0', cal.substract(1, 1));
+    })
+  }),
+
+  describe('multiply', function(){
+    it('multiply', function(){
+      assert.equal('10', cal.multiply(2, 5));
+    })
+  }),
+
+  describe('divide', function(){
+    it('divide', function(){
+      assert.equal('2', cal.divide(4, 2));
+    })
+  }),
+
+  describe('mod', function(){
+    it('mod', function(){
+      assert.equal('2', cal.mod(5, 3));
+    })
   })
-})
+});
