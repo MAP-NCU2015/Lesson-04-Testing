9d818b9650dfe423fb15e216436fb9576810123c
DannyJJJJ <danny89019@gmail.com>
2015-10-30 12:25:09 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -7,9 +7,18 @@ function add(num1, num2){
   return num1 + num2;
 }
 
+function multiply(num1, num2){
+  return num1 * num2;
+}
+function div(num1, num2){
+  return num1 / num2;
+}
 function substract(num1, num2){
   return num1 - num2;
 }
 
 module.exports.add = add;
+module.exports.div = div;
+module.exports.mult = mult;
+module.exports.substract = substract;
 module.exports.isNumber = isNumber;
