abf7816681998bcab892fd3c917d194de1cb3871
Sam Huang <sam40313@yahoo.com.tw>
2015-10-30 14:28:49 +0800

    add new feature and test negative path

--- calculator.js
+++ calculator.js
@@ -11,5 +11,17 @@ function substract(num1, num2){
   return num1 - num2;
 }
 
+function multiple(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+
 module.exports.add = add;
 module.exports.isNumber = isNumber;
+module.exports.substract = substract;
+module.exports.multiple = multiple;
+module.exports.divide = divide;
