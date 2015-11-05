0d3be437399e34af0bb7159b4a7379060567a1e7
林彥礦 <gmkevin00@gmail.com>
2015-11-06 01:01:10 +0800

    1106 Lesson4 finished

--- test/test_calculator.js
+++ test/test_calculator.js
@@ -3,8 +3,18 @@ var cal = require('../calculator.js')
 
 describe('Calculator', function(){
   describe('add', function(){
-    it('add', function(){
-      assert.equal('2', cal.add(1, 1));
+    it('will add', function(){
+      assert.equal('18', cal.add(12, 6));
+    })
+	it('will substract', function(){
+      assert.equal('6', cal.substract(12, 6));
+    })
+	it('will multiple', function(){
+      assert.equal('72', cal.multiple(12, 6));
+    })
+	it('will divide', function(){
+	  //this will be wrong
+      assert.equal('3', cal.divide(12, 6));
     })
   })
 })
